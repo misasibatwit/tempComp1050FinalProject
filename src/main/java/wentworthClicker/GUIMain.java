fc2c28688eb5acc48e70c24c14204998bb15a958
misasib <misasib@spotty-ubuntu>
2026-03-27 16:10:05 -0400

    test

---
+++ src/main/java/wentworthClicker/GUIMain.java
@@ -0,0 +1,23 @@
+package wentworthClicker;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIMain extends Application {
+
+	public GUIMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
